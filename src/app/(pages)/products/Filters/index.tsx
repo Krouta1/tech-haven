63731c7b6530b82c4af49ab00cb49d715bb1fc67
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-12-16 08:56:09 +0100

    Products

---
+++ src/app/(pages)/products/Filters/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './index.module.scss'
+
+type Props = {}
+
+const Filters = (props: Props) => {
+  return (
+    <div>Filters</div>
+  )
+}
+
+export default Filters
